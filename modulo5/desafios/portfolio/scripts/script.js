ca23621bda057fcd5cd807ac22abda95d71f2765
Kauã Cavalcante <cavalcante.kauamoraes@gmail.com>
2026-03-30 12:19:16 -0300

    ajustes e correcoes no header

--- modulo5/desafios/portfolio/scripts/script.js
+++ modulo5/desafios/portfolio/scripts/script.js
@@ -1,4 +1,10 @@
 function alterarTema() {
     const html = document.documentElement;
-    html.classList.toggle('tema-escuro')
+    const botaoTema = document.querySelector("#botao-tema")
+    html.classList.toggle('tema-escuro');
+    if(botaoTema.innerHTML == 'dark_mode') {
+        botaoTema.innerHTML = 'light_mode'
+    } else {
+        botaoTema.innerHTML = 'dark_mode'
+    }
 }
